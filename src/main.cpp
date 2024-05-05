0c48fd22a86ebe93be16e6a4f6028a3eee8c1f03
Andrew De La Fuente <delafuenteandrew@ymail.com>
2024-05-04 20:50:05 -0700

    changed pins and used 4 digit display for counting

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 
 enum digits {zero,one,two,three,four,five,six,seven,eight,nine};
 bool b_decimal = false;
+int delay_time = 3;
+#define DIGIT_1_PIN 9
+#define DIGIT_2_PIN 8
+#define DIGIT_3_PIN 7
+#define DIGIT_4_PIN 6
+#define ROW_1 0
+#define COLUMN 1
 
 //Forward declare func
 int digit_sequence(digits num,bool decimal=false);
@@ -11,19 +18,55 @@ void setup() {
   pinMode(11,OUTPUT);
   pinMode(13,OUTPUT);
   pinMode(10,OUTPUT);
+  pinMode(DIGIT_1_PIN,INPUT);
+  pinMode(DIGIT_2_PIN,INPUT);
+  pinMode(DIGIT_3_PIN,INPUT);
+  pinMode(DIGIT_4_PIN,INPUT);
+  pinMode(ROW_1,INPUT_PULLUP);
+  pinMode(COLUMN,OUTPUT);
   //SPCR=0b01010010;
   SPCR=0b01110010; // LSB MODE, easier base
   SPSR|=0b00000001;
   digitalWrite(10,HIGH);
+  // set input pins too low by default
+  pinMode(DIGIT_1_PIN, LOW);
+  pinMode(DIGIT_2_PIN, LOW);
+  pinMode(DIGIT_3_PIN,LOW);
+  pinMode(DIGIT_4_PIN, LOW);
 }
 
 void loop() {
+  // count 
+  for(int i = 0; i < 10000; i++)
+  {
+    int digit1 = i / 1000 % 10;
+    int digit2 = i / 100 % 10;
+    int digit3 = i / 10 % 10;
+    int digit4 = i % 10;
 
-  for(int i = 0; i < 10; i++){ // 
-    update_7segmentDisplay((digits)i,false);
-    delay (1000);
+    // hold value for 600 ms
+    for(int i = 0; i < 600/(4*delay_time); i++){
+      pinMode(DIGIT_1_PIN,HIGH);
+      update_7segmentDisplay((digits)digit4,false);
+      delay(delay_time);
+      pinMode(DIGIT_1_PIN, LOW);
+
+      pinMode(DIGIT_2_PIN,HIGH);
+      update_7segmentDisplay((digits)digit3,false);
+      delay(delay_time);
+      pinMode(DIGIT_2_PIN, LOW);
+
+      pinMode(DIGIT_3_PIN,HIGH);
+      update_7segmentDisplay((digits)digit2,false);
+      delay(delay_time);
+      pinMode(DIGIT_3_PIN, LOW);
+
+      pinMode(DIGIT_4_PIN,HIGH);
+      update_7segmentDisplay((digits)digit1,false);
+      delay(delay_time);
+      pinMode(DIGIT_4_PIN, LOW);
+    }
   }
-
 }
 
 // Functions
